7a3be49943cce6f542b04a12554a1e29488c35d4
Keenn Howard Caw <kcaw@isbx.com>
2021-09-01 08:27:54 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
